f936c4b4bc2c02e852dbb269f0e48717e9a27a97
abenezerayalew <aben.man.unity@gmail.com>
2023-07-13 23:19:03 +0300

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python3
 """ Minimum Operations """
 def minOperations(n):
-    """ Minimum Operations """
-    if n <= 1:
+    '''Computes the fewest number of operations needed to result
+    in exactly n H characters.
+    '''
+    if not isinstance(n, int):
         return 0
-    else:
-        i = 2
-        count = 0
-        while i <= n:
-            if n % i == 0:
-                count += i
-                n = n / i
-            else:
-                i += 1
-        return count
-
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
