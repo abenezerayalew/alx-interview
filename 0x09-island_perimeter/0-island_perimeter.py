aad3b6ce3d395c815e763eedc88bd71cc12ab057
abenezerayalew <aben.man.unity@gmail.com>
2023-09-01 00:11:33 +0300

    completed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -2,15 +2,16 @@
 """Island perimeter computing module.
 """
 
+
 def island_perimeter(grid):
     """Computes the perimeter of an island with no lakes.
     """
     if not isinstance(grid, list) or not grid:
         return 0
-    
+
     perimeter = 0
     n, m = len(grid), len(grid[0])
-    
+
     for i in range(n):
         for j in range(m):
             if grid[i][j] == 1:
@@ -21,5 +22,5 @@ def island_perimeter(grid):
                     (j == 0 or grid[i][j - 1] == 0),
                 ]
                 perimeter += sum(edges)
-                
+
     return perimeter
